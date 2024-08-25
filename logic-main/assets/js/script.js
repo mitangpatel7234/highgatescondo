f670743a2da0d14a0d8b6faf0c21c166d0350dea
mitangpatel7234 <mitangpatel7234@gmail.com>
2024-08-25 20:02:42 +0530

    first commit

---
+++ logic-main/assets/js/script.js
@@ -0,0 +1,25 @@
+// jQuery(window).scroll(function () {
+//     var top = jQuery(document).scrollTop();
+//     var height = 300;
+//     //alert(batas);
+
+//     if (top > height) {
+//         jQuery('.navbar-fixed-top').addClass('menu-scroll');
+//         jQuery('.navbar-brand img').attr('src', 'assets/images/truman_logo_2019.png');
+
+//     } else {
+//         jQuery('.navbar-fixed-top').removeClass('menu-scroll');
+//         jQuery('.navbar-brand img').attr('src', 'assets/images/truman_white_main_logo.png');
+//     }
+    
+// });
+
+  
+
+  function openVideoPlayer() {
+    document.getElementById('video-player').style.display = 'block';
+}
+
+function closeVideoPlayer() {
+    document.getElementById('video-player').style.display = 'none';
+}
